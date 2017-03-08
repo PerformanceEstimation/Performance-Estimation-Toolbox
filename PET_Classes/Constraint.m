7edb2ec8af3a58a553c726b63309ba9057e0bed6
Taylor <adrien.taylor@gmail.com>
2017-03-08 11:27:48 +0100

    basic files - second version

---
+++ PET_Classes/Constraint.m
@@ -0,0 +1,46 @@
+classdef Constraint < handle
+    properties (GetAccess=private)
+        expr;
+        signtype;
+        next;
+    end
+    methods
+        function obj=Constraint(expr,signtype)
+            assert(isa(expr,'Evaluable'),'Constraints must involve scalar values only (PET class: Constraint)');
+            assert(strcmp(expr.getType(),'Function value'),'Constraints must involve scalar values only (PET class: Constraint)');
+            assert(strcmp(signtype,'le') || strcmp(signtype,'ge') || strcmp(signtype,'eq'), 'This is not a valid constraint (PET class: Constraint)');
+            obj.signtype=signtype;
+            obj.expr=expr;
+            obj.next=[];
+        end
+        function cons=Eval(obj)
+            exprm=obj.expr.Eval();
+            cons=[];
+            if ~isa(exprm,'double')
+                switch obj.signtype
+                    case 'le'
+                        cons=(exprm<=0);
+                    case 'ge'
+                        cons=(exprm>=0);
+                    case 'eq'
+                        cons=(exprm==0);
+                end
+            else
+                if (exprm~=0 && strcmp(obj.signtype,'eq')) || (exprm>0 && strcmp(obj.signtype,'le')) || (exprm<0 && strcmp(obj.signtype,'ge'))
+                    assert(false,'Warning, some constraints should be checked (infeasible with no dependence in the variables)');
+                end
+            end
+            if ~isempty(obj.next)
+                cons=cons+obj.next.Eval();
+            end
+        end
+        function cons=plus(a,b)
+            assert(isa(a,'Constraint') & isa(b,'Constraint'),'Concatenation must involve constraints only (PET class: Constraint)');
+            cons=a;
+            a.next=b;
+        end
+        function disp(obj)
+            fprintf('PET Constraint\n');
+        end
+    end
+end
