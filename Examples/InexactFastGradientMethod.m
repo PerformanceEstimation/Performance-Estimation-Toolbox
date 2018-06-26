3a189d5b9d1813df7e9f38fa349bf9da04dd3af2
Adrien Taylor <adrien.taylor@gmail.com>
2018-06-26 16:36:44 +0200

    Update InexactFastGradientMethod.m
    Update InexactFastGradientMethod.m
    
    Update InexactFastGradientMethod.m
    
    Update InexactFastGradientMethod.m
    
    Update InexactFastGradientMethod.m
    
    Update InexactFastGradientMethod.m
    
    Update InexactFastGradientMethod.m
    
    Update InexactFastGradientMethod.m

--- Examples/InexactFastGradientMethod.m
+++ Examples/InexactFastGradientMethod.m
@@ -15,41 +15,41 @@ clear all; clc;
 
 
 % (0) Initialize an empty PEP
-P=pep();
+P = pep();
 
 % (1) Set up the objective function
-param.mu=0;
-param.L=1;      % Smoothness parameter
+param.mu = 0;      % strong convexity parameter
+param.L  = 1;      % Smoothness parameter
 
 F=P.DeclareFunction('SmoothStronglyConvex',param); % F is the objective function
 
 % (2) Set up the starting point and initial condition
-x0=P.StartingPoint();             % x0 is some starting point
-[xs,fs]=F.OptimalPoint();              % xs is an optimal point, and fs=F(xs)
-P.InitialCondition((x0-xs)^2<=1); % Add an initial condition ||x0-xs||^2<= 1
+x0       = P.StartingPoint();        % x0 is some starting point
+[xs, fs] = F.OptimalPoint();         % xs is an optimal point, and fs=F(xs)
+P.InitialCondition((x0-xs)^2 <= 1);  % Add an initial condition ||x0-xs||^2<= 1
 
 % (3) Algorithm
-N=7;		% number of iterations
+N = 7; % number of iterations
 
-x=cell(N+1,1);% we store the iterates in a cell for convenience
-x{1}=x0;
-y=x0;
-eps=.1;
-for i=1:N
-    d  = inexactsubgradient(y,F,eps);
-    x{i+1}=y-1/param.L*d;
-    y=x{i+1}+(i-1)/(i+2)*(x{i+1}-x{i});
+x    = cell(N+1,1); % we store the iterates in a cell for convenience
+x{1} = x0;
+y    = x0;
+eps  = .1;
+for i = 1:N
+    d      = inexactsubgradient(y, F, eps);
+    x{i+1} = y - 1/param.L * d;
+    y      = x{i+1} + (i-1)/(i+2) * (x{i+1} - x{i});
 end
 
 % (4) Set up the performance measure
-[g,f]=F.oracle(x{N+1});                % g=grad F(x), f=F(x)
-P.PerformanceMetric(f-fs); % Worst-case evaluated as F(x)-F(xs)
+[g, f] = F.oracle(x{N+1});    % g=grad F(x), f=F(x)
+P.PerformanceMetric(f - fs); % Worst-case evaluated as F(x)-F(xs)
 
 % (5) Solve the PEP
 P.solve()
 
 % (6) Evaluate the output
-double(f-fs)   % worst-case objective function accuracy
+double(f - fs)   % worst-case objective function accuracy
 
 % Result should be worse than 2/(N^2+5*N+6) (for exact fast gradient)
 % see Taylor, Adrien B., Julien M. Hendrickx, and François Glineur.
